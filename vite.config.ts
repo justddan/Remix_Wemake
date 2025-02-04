182cc62d5c034c9311bb52e71d032a0babaab94d
김동규 <dgorzldd@gmail.com>
2025-02-04 12:19:46 +0900

    초기 프로젝트 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths(), react()],
+  build: {
+    cssMinify: true,
+    ssr: false,
+  },
+  server: {
+    port: 3000,
+  },
 });
