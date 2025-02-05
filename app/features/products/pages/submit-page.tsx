d9a933937bcf0492cfc6e2fdc18f208dac5cf453
김동규 <dgorzldd@gmail.com>
2025-02-05 18:16:37 +0900

    SubmitPage 퍼블리시

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -10,6 +10,10 @@ import {
   SelectValue,
 } from "~/common/components/ui/select";
 import SelectPair from "~/common/components/select-pair";
+import { Input } from "~/common/components/ui/input";
+import { Label } from "~/common/components/ui/label";
+import { useState } from "react";
+import { Button } from "~/common/components/ui/button";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -19,6 +23,13 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function SubmitPage() {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setIcon(URL.createObjectURL(file));
+    }
+  };
   return (
     <div>
       <Hero
@@ -74,6 +85,43 @@ export default function SubmitPage() {
               { value: "category2", label: "Category 2" },
             ]}
           />
+          <Button type="submit" className="w-full" size="lg">
+            Submit
+          </Button>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <div className="size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ? (
+              <img
+                src={icon}
+                alt="icon"
+                className="w-full h-full object-cover"
+              />
+            ) : null}
+          </div>
+          <Label className="flex flex-col gap-1">
+            Icon
+            <small className="text-muted-foreground">
+              This is the icon of your product
+            </small>
+          </Label>
+          <Input
+            type="file"
+            className="w-1/2"
+            onChange={onChange}
+            required
+            name="icon"
+            multiple
+          />
+          <div className="flex flex-col text-xs">
+            <span className="text-muted-foreground">
+              Recommended size: 128x128px
+            </span>
+            <span className="text-muted-foreground">
+              Allowed Formats: PNG, JPEG
+            </span>
+            <span className="text-muted-foreground">Max file size: 1MB</span>
+          </div>
         </div>
       </Form>
     </div>
