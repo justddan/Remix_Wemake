0e88beea2f0543f17e803260e8c27ddf6d50ecaa
김동규 <dgorzldd@gmail.com>
2025-02-05 21:53:44 +0900

    layoutRoute / NavLink

--- app/features/products/pages/product-overview-page.tsx
+++ app/features/products/pages/product-overview-page.tsx
@@ -14,41 +14,7 @@ export default function ProductOverviewPage({
   params: { productId },
 }: Route.ComponentProps) {
   return (
-    <div className=" space-y-10">
-      <div className="flex justify-between">
-        <div className="flex gap-10">
-          <div className="size-40 rounded-xl shadow-xl bg-primary/50"></div>
-          <div>
-            <h1 className="text-5xl font-bold">Product Name</h1>
-            <p className="text-2xl font-light">Product Description</p>
-            <div className="mt-5 flex items-center gap-2">
-              <div className="flex text-yellow-400">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <StarIcon className="size-4" fill="currentColor" />
-                ))}
-              </div>
-              <span className="text-muted-foreground">100 reviews</span>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-5">
-          <Button variant="secondary" size="lg" className="text-lg h-14 px-10">
-            Visit Website
-          </Button>
-          <Button size="lg" className="text-lg h-14 px-10">
-            <ChevronUpIcon className="size-4" />
-            Upvote (100)
-          </Button>
-        </div>
-      </div>
-      <div className="flex gap-2.5">
-        <Button variant={"outline"} asChild>
-          <Link to={`/products/${productId}/overview`}>Overview</Link>
-        </Button>
-        <Button variant={"outline"} asChild>
-          <Link to={`/products/${productId}/reviews`}>Reviews</Link>
-        </Button>
-      </div>
+    <div>
       <div className="space-y-10">
         <div className="space-y-1">
           <h3 className="text-lg font-bold">What is this product?</h3>
