a8f2ae41d823350949b712a2b73da3a5941fbba0
김동규 <dgorzldd@gmail.com>
2025-02-05 16:49:42 +0900

    CategoryPage 퍼블리시

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,6 +1,16 @@
-import { type MetaFunction } from "react-router";
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/category-page";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import { ChevronRightIcon } from "lucide-react";
+import { Link } from "react-router";
+import { CategoryCard } from "../components/category-card";
 
-export const meta: MetaFunction = () => {
+export const meta: Route.MetaFunction = () => {
   return [
     { title: "Categories | wemake" },
     { name: "description", content: "Browse products by category" },
@@ -9,8 +19,18 @@ export const meta: MetaFunction = () => {
 
 export default function CategoriesPage() {
   return (
-    <div>
-      <h1>Categories Page</h1>
+    <div className="space-y-10">
+      <Hero title="Categories" subtitle="Browse products by category" />
+      <div className="grid grid-cols-4 gap-10">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CategoryCard
+            key={`categoryId-${index}`}
+            id="categoryId"
+            name="Category Name"
+            description="Category Description"
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- app/features/products/pages/category-page.tsx
+++ app/features/products/pages/category-page.tsx
@@ -1,16 +1,37 @@
-import { type MetaFunction } from "react-router";
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/category-page";
+import { ProductCard } from "../compnents/product-card";
+import { ProductPagination } from "~/common/components/product-pagination";
 
-export const meta: MetaFunction = () => {
+export const meta: Route.MetaFunction = ({ params }) => {
   return [
-    { title: "Category | wemake" },
-    { name: "description", content: "Products in this category" },
+    { title: `Developer Tools | wemake` },
+    { name: "description", content: "Browse Developer Tools products" },
   ];
 };
 
 export default function CategoryPage() {
   return (
-    <div>
-      <h1>Category Page</h1>
+    <div className="space-y-10">
+      <Hero
+        title="Developer Tools"
+        subtitle="Tools for developers to build products faster"
+      />
+
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            key={`productId-${index}`}
+            id="productId"
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
 }
