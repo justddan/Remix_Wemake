b90c1402e07b47b304e184a3cafbb8c94cb9bb4c
김동규 <dgorzldd@gmail.com>
2025-02-05 21:38:41 +0900

    Product Overview Page 퍼블리시

---
+++ app/features/products/pages/new-product-review-page.tsx
@@ -0,0 +1,7 @@
+import type { Route } from "./+types/new-product-review-page";
+
+export default function NewProductReviewPage({
+  loaderData,
+}: Route.ComponentProps) {
+  return <div></div>;
+}
---
+++ app/features/products/pages/product-reviews-page.tsx
@@ -0,0 +1,7 @@
+import type { Route } from "./+types/product-reviews-page";
+
+export default function ProductReviewsPage({
+  loaderData,
+}: Route.ComponentProps) {
+  return <div></div>;
+}
