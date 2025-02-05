620d872c2a95a98ac6ae53aa7a68f25eedc45b32
김동규 <dgorzldd@gmail.com>
2025-02-05 17:49:37 +0900

    Input과 Select 컴포넌트 만들기

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -1,6 +1,17 @@
-import { type MetaFunction } from "react-router";
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/submit-page";
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/common/components/ui/select";
+import SelectPair from "~/common/components/select-pair";
 
-export const meta: MetaFunction = () => {
+export const meta: Route.MetaFunction = () => {
   return [
     { title: "Submit Product | wemake" },
     { name: "description", content: "Submit your product" },
@@ -10,7 +21,61 @@ export const meta: MetaFunction = () => {
 export default function SubmitPage() {
   return (
     <div>
-      <h1>Submit Page</h1>
+      <Hero
+        title="Submit Product"
+        subtitle="Share your product with the world"
+      />
+      <Form className="grid grid-cols-2 gap-10 max-w-screen-lg mx-auto">
+        <div className="space-y-5">
+          <InputPair
+            label="Name"
+            description="This is the name of your product"
+            id="name"
+            name="name"
+            required
+            placeholder="Name of your product"
+          />
+          <InputPair
+            label="Tagline"
+            description="60 characters or less"
+            id="tagline"
+            name="tagline"
+            required
+            type="text"
+            placeholder="A concise description of your product"
+          />
+          <InputPair
+            label="URL"
+            description="The URL of your product"
+            id="url"
+            name="url"
+            required
+            type="text"
+            placeholder="https://example.com"
+          />
+          <InputPair
+            label="Description"
+            description="A detailed description of your product"
+            id="description"
+            name="description"
+            required
+            textArea
+            type="text"
+            placeholder="A detailed description of your product"
+          />
+          <SelectPair
+            label="Category"
+            description="The category of your product"
+            name="category"
+            required
+            placeholder="Select a category"
+            options={[
+              { value: "category1", label: "Category 1" },
+              { value: "category2", label: "Category 2" },
+            ]}
+          />
+        </div>
+      </Form>
     </div>
   );
 }
