960fc8cbddbe715445f585b1f6a2227d3592ffac
김동규 <dgorzldd@gmail.com>
2025-02-05 14:02:33 +0900

    Data 유효성 검사

--- app/features/products/pages/daily-leaderboards-page.tsx
+++ app/features/products/pages/daily-leaderboards-page.tsx
@@ -1,5 +1,7 @@
-import { type MetaFunction } from "react-router";
-
+import { data, isRouteErrorResponse, type MetaFunction } from "react-router";
+import type { Route } from "./+types/daily-leaderboards-page";
+import { DateTime } from "luxon";
+import { z } from "zod";
 export const meta: MetaFunction = () => {
   return [
     { title: "Daily Leaderboards | WeMake" },
@@ -7,10 +9,69 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function DailyLeaderboardsPage() {
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
+
+export const loader = async ({ params }: Route.LoaderArgs) => {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "invalid_params",
+        message: "Invalid params",
+      },
+      { status: 400 }
+    );
+  }
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+  if (!date.isValid) {
+    throw data(
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      { status: 400 }
+    );
+  }
+
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today) {
+    throw data(
+      {
+        error_code: "future_date",
+        message: "Future date",
+      },
+      { status: 400 }
+    );
+  }
+  return {
+    date,
+  };
+};
+
+export default function DailyLeaderboardsPage({
+  loaderData,
+}: Route.ComponentProps) {
   return (
-    <div>
+    <div className="container mx-auto px-4 py-8">
       <h1>Daily Leaderboards Page</h1>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown error</div>;
 }
