a16090e1999352f37474fee5d5ecada2f35c2e80
김동규 <dgorzldd@gmail.com>
2025-02-05 22:27:04 +0900

    CreateReview Dialog

--- app/features/products/pages/new-product-review-page.tsx
+++
@@ -1,7 +0,0 @@
-import type { Route } from "./+types/new-product-review-page";
-
-export default function NewProductReviewPage({
-  loaderData,
-}: Route.ComponentProps) {
-  return <div></div>;
-}
--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,6 +1,15 @@
 import { Button } from "~/common/components/ui/button";
 import type { Route } from "./+types/product-reviews-page";
 import { ReviewCard } from "../components/review-card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/common/components/ui/dialog";
+import CreateReviewDialog from "../components/create-review-dialog";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -11,24 +20,29 @@ export const meta: Route.MetaFunction = () => {
 
 export default function ProductReviewsPage() {
   return (
-    <div className="space-y-10 max-w-xl">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold">10 Reviews</h2>
-        <Button variant={"secondary"}>Write a Review</Button>
+    <Dialog>
+      <div className="space-y-10 max-w-xl">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">10 Reviews</h2>
+          <DialogTrigger>
+            <Button variant={"secondary"}>Write a Review</Button>
+          </DialogTrigger>
+        </div>
+        <div className="space-y-20">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ReviewCard
+              key={index}
+              username="John Doe"
+              handle="username"
+              rating={5}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+              avatarUrl="https://github.com/shadcn.png"
+              postedAt="10 days ago"
+            />
+          ))}
+        </div>
       </div>
-      <div className="space-y-20">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <ReviewCard
-            key={index}
-            username="John Doe"
-            handle="username"
-            rating={5}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-            avatarUrl="https://github.com/shadcn.png"
-            postedAt="10 days ago"
-          />
-        ))}
-      </div>
-    </div>
+      <CreateReviewDialog />
+    </Dialog>
   );
 }
