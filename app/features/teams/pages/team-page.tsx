7101c7e7dcedc7eafda3a33939e914e8d1926596
김동규 <dgorzldd@gmail.com>
2025-02-06 19:56:43 +0900

    Team Page 퍼블리시

--- app/features/teams/pages/team-page.tsx
+++ app/features/teams/pages/team-page.tsx
@@ -1,4 +1,20 @@
+import { Hero } from "~/common/components/hero";
 import type { Route } from "./+types/team-page";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import { Badge } from "~/common/components/ui/badge";
+import { Button } from "~/common/components/ui/button";
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -8,5 +24,100 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function TeamPage() {
-  return <div>Team Page</div>;
+  return (
+    <div className="space-y-20">
+      <Hero title="Join lynn's team" />
+      <div className="grid grid-cols-6 gap-40 items-start">
+        <div className="col-span-4 grid grid-cols-4 gap-5">
+          {[
+            {
+              title: "Product name",
+              value: "Doggile Social",
+            },
+            {
+              title: "Stage",
+              value: "MVP",
+            },
+            {
+              title: "Team Size",
+              value: 3,
+            },
+            {
+              title: "Available Equity",
+              value: 50,
+            },
+          ].map((item) => (
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-sm font-medium text-muted-foreground">
+                  {item.title}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="p-0 font-bold text-2xl">{item.value}</p>
+              </CardContent>
+            </Card>
+          ))}
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Looking for
+              </CardTitle>
+              <CardContent className="p-0 font-bold text-2xl">
+                <ul className="text-lg list-disc list-inside">
+                  {[
+                    "React Developer",
+                    "Backend Developer",
+                    "Product Manager",
+                    "UI/UX Designer",
+                  ].map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Idea Description
+              </CardTitle>
+              <CardContent className="p-0 font-medium text-xl">
+                <p>
+                  Doggile Social is a social media platform for dogs. It allows
+                  dogs to connect with each other and share their thoughts and
+                  experiences.
+                </p>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+        <aside className="col-span-2 space-y-5 border rounded-lg p-6 shadow-sm">
+          <div className="flex gap-5 ">
+            <Avatar className="size-14">
+              <AvatarImage src="https://github.com/inthetiger.png" />
+              <AvatarFallback>N</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <h4 className="text-lg font-medium">Lynn</h4>
+              <Badge variant="secondary">Entrepreneur</Badge>
+            </div>
+          </div>
+          <Form className="space-y-5">
+            <InputPair
+              label="Introduce yourself"
+              description="Tell us about yourself"
+              name="introduction"
+              type="text"
+              id="introduction"
+              required
+              textArea
+              placeholder="i.e. I'm a React Developer with 5 years of experience"
+            />
+            <Button className="w-full">Get in touch</Button>
+          </Form>
+        </aside>
+      </div>
+    </div>
+  );
 }
