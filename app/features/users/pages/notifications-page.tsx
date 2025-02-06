2b83221820991e9eb009a1c013c0dd43df8bba21
김동규 <dgorzldd@gmail.com>
2025-02-06 20:44:07 +0900

    Notification Page 퍼블리시

--- app/features/users/pages/notifications-page.tsx
+++ app/features/users/pages/notifications-page.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/notifications-page";
+import { NotificationCard } from "../components/notification-card";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -9,5 +10,19 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function NotificationsPage() {
-  return <div></div>;
+  return (
+    <div className="space-y-20">
+      <h1 className="text-4xl font-bold">Notifications</h1>
+      <div className="flex flex-col items-start gap-5">
+        <NotificationCard
+          avatarUrl="https://github.com/shadcn.png"
+          avatarFallback="S"
+          username="Steve Jobs"
+          message=" followed you."
+          timestamp="2 days ago"
+          seen={false}
+        />
+      </div>
+    </div>
+  );
 }
