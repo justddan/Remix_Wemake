61e63650405e465dc5e14ddd252cf597ae5d4ccd
김동규 <dgorzldd@gmail.com>
2025-02-07 13:26:22 +0900

    Dashboard Ideas Page 퍼블리시

--- app/features/users/pages/dashboard-ideas-page.tsx
+++ app/features/users/pages/dashboard-ideas-page.tsx
@@ -1,3 +1,4 @@
+import { IdeaCard } from "~/features/ideas/components/idea-card";
 import type { Route } from "./+types/dashboard-ideas-page";
 
 export const meta: Route.MetaFunction = () => {
@@ -13,5 +14,21 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function DashboardIdeasPage() {
-  return <div></div>;
+  return (
+    <div className="space-y-5 h-full">
+      <h1 className="text-2xl font-semibold mb-6">Claimed Ideas</h1>
+      <div className="grid grid-cols-4 gap-6">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <IdeaCard
+            key={`ideaId-${index}`}
+            id="ideaId"
+            title="A startup that creates an AI-powered generated personal trainer, delivering customized fitness recommendations and tracking of progress using a mobile app to track workouts and progress as well as a website to manage the bussiness."
+            viewCount={123}
+            createdAt="12 hours ago"
+            likeCount={12}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
