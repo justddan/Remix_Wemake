da834a9d5ee97d49a13141667587d082ac92416a
김동규 <dgorzldd@gmail.com>
2025-03-25 10:41:30 +0900

    Notifiaction Trigger

---
+++ app/sql/migrations/0015_red_iron_lad.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "posts" ALTER COLUMN "topic_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "profile_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "reviews" ALTER COLUMN "product_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "reviews" ALTER COLUMN "profile_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "follows" ALTER COLUMN "follower_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "follows" ALTER COLUMN "following_id" SET NOT NULL;
