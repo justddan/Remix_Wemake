991ca33b672c224b1ebcb027dc1cf87fbdc0069b
김동규 <dgorzldd@gmail.com>
2025-03-31 13:40:00 +0900

    토스 페이먼츠 위젯

--- app/features/products/pages/promote-page.tsx
+++ app/features/products/pages/promote-page.tsx
@@ -4,10 +4,14 @@ import { Form } from "react-router";
 import SelectPair from "~/common/components/select-pair";
 import { Calendar } from "~/common/components/ui/calendar";
 import { Label } from "~/common/components/ui/label";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { DateRange } from "react-day-picker";
 import { DateTime } from "luxon";
 import { Button } from "~/common/components/ui/button";
+import {
+  loadTossPayments,
+  type TossPaymentsWidgets,
+} from "@tosspayments/tosspayments-sdk";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -28,52 +32,93 @@ export default function PromotePage() {
           "days"
         ).days
       : 0;
+
+  const widgets = useRef<TossPaymentsWidgets | null>(null);
+  useEffect(() => {
+    const initToss = async () => {
+      const toss = loadTossPayments("test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm");
+      widgets.current = (await toss).widgets({
+        customerKey: "1111111",
+      });
+      await widgets.current.setAmount({
+        value: 10000,
+        currency: "KRW",
+      });
+      await widgets.current.renderPaymentMethods({
+        selector: "#toss-payment-methods",
+      });
+      await widgets.current.renderAgreement({
+        selector: "#toss-payment-agreement",
+      });
+    };
+    initToss();
+  }, []);
+
+  useEffect(() => {
+    if (widgets.current) {
+      widgets.current.setAmount({
+        value: totalDays * 20000,
+        currency: "KRW",
+      });
+    }
+  }, [totalDays]);
   return (
     <div>
       <Hero
         title="Promote your product"
         subtitle="Boost your product's visibility."
       />
-      <Form className="max-w-sm mx-auto flex flex-col gap-10 items-center">
-        <SelectPair
-          label="Select a product"
-          description="Select a product to promote"
-          name="product"
-          placeholder="Select a product"
-          options={[
-            {
-              label: "AI Dark Mode Maker",
-              value: "ai-dark-mode-maker",
-            },
-            {
-              label: "AI Dark Mode Maker",
-              value: "ai-dark-mode-maker-1",
-            },
-            {
-              label: "AI Dark Mode Maker",
-              value: "ai-dark-mode-maker-2",
-            },
-          ]}
-        />
-        <div className="flex flex-col gap-2 items-center w-full">
-          <Label className="flex flex-col gap-1">
-            Select a range of dates for promotion
-            <small className="text-muted-foreground text-center">
-              Minimum duration is 3 days
-            </small>
-          </Label>
-          <Calendar
-            mode="range"
-            selected={promotionPeriod}
-            onSelect={setPromotionPeriod}
-            min={3}
-            disabled={{ before: new Date() }}
+      <div className="grid grid-cols-6 gap-10">
+        <Form className="col-span-3 mx-auto w-1/2 flex flex-col gap-10 items-start">
+          <SelectPair
+            label="Select a product"
+            description="Select a product to promote"
+            name="product"
+            placeholder="Select a product"
+            options={[
+              {
+                label: "AI Dark Mode Maker",
+                value: "ai-dark-mode-maker",
+              },
+              {
+                label: "AI Dark Mode Maker",
+                value: "ai-dark-mode-maker-1",
+              },
+              {
+                label: "AI Dark Mode Maker",
+                value: "ai-dark-mode-maker-2",
+              },
+            ]}
           />
-        </div>
-        <Button disabled={totalDays === 0}>
-          Go to checkout (${totalDays * 20})
-        </Button>
-      </Form>
+          <div className="flex flex-col gap-2 items-center w-full">
+            <Label className="flex flex-col gap-1">
+              Select a range of dates for promotion
+              <small className="text-muted-foreground text-center">
+                Minimum duration is 3 days
+              </small>
+            </Label>
+            <Calendar
+              mode="range"
+              selected={promotionPeriod}
+              onSelect={setPromotionPeriod}
+              min={3}
+              disabled={{ before: new Date() }}
+            />
+          </div>
+        </Form>
+        <aside className="col-span-3 px-20 flex flex-col items-center">
+          <div id="toss-payment-methods" className="w-full" />
+          <div id="toss-payment-agreement" />
+          <Button className="w-full" disabled={totalDays === 0}>
+            Checkout (
+            {(totalDays * 20000).toLocaleString("ko-KR", {
+              style: "currency",
+              currency: "KRW",
+            })}
+            )
+          </Button>
+        </aside>
+      </div>
     </div>
   );
 }
